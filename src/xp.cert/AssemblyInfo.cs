e6f23bb7421229202940ed75d8b39de48f35931c
Timm Friebe <thekid@thekid.de>
2016-06-05 19:44:48 +0200

    Release 0.1.0

--- src/xp.cert/AssemblyInfo.cs
+++ src/xp.cert/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("XP-Framework Team")]
 [assembly: AssemblyProduct("XP Certificate Management")]
 [assembly: AssemblyCopyright("Copyright XP-Framework Team 2001-2016")]
-[assembly: AssemblyVersion("1.0.0.1901")]
+[assembly: AssemblyVersion("0.1.0.1912")]
 [assembly: TargetFramework(".NETFramework,Version=v5.0")]
